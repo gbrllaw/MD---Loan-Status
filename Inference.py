6b656475484b30fb9a7b55d10857af9a4a81d540
gbrllaw <geboy2519@gmail.com>
2025-04-22 09:25:12 +0700

    Update Inference.py

--- Inference.py
+++ Inference.py
@@ -1,40 +1,35 @@
 import joblib
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import RobustScaler
 
-# Inisialisasi class untuk prediksi
 class LoanXGBoostModelInference:
-    def __init__(self, model_path, scaler_path, columns_path):
-        # Memuat model, scaler, dan struktur kolom
+    def __init__(self, model_path, scaler_path, imputer_path, columns_path, encoders_path):
         self.model = joblib.load(model_path)
         self.scaler = joblib.load(scaler_path)
+        self.imputer = joblib.load(imputer_path)
         self.columns = joblib.load(columns_path)
-        
+        self.encoders = joblib.load(encoders_path)
+
     def preprocess_new_data(self, new_data):
-        # Pastikan kolom 'loan_status' tidak ada di data yang dikirimkan
         features = new_data.copy()
         if 'loan_status' in features.columns:
-            features = features.drop('loan_status', axis=1)  # Drop 'loan_status' untuk input fitur
+            features = features.drop('loan_status', axis=1)
 
-        # preprocessing
+        # Cleaning data
         features['person_gender'] = features['person_gender'].str.lower()
         features['person_gender'] = features['person_gender'].replace('fe male', 'female')
 
-        # Imputasi missing value
-        imputer = SimpleImputer(strategy='median')
-        features['person_income'] = imputer.fit_transform(features[['person_income']])
+        # Imputasi nilai kosong
+        features['person_income'] = self.imputer.transform(features[['person_income']])
 
         # Scaling numerik
         numeric_cols = features.select_dtypes(include=['int64', 'float64']).columns
-        features[numeric_cols] = self.scaler.transform(features[numeric_cols])
+        features.loc[:, numeric_cols] = self.scaler.transform(features[numeric_cols])
 
         # Encoding binary kategorikal
         label_cols = ['person_gender', 'previous_loan_defaults_on_file']
         for col in label_cols:
-            encoder = LabelEncoder()
-            features[col] = encoder.fit_transform(features[col])
+            encoder = self.encoders[col]
+            features[col] = encoder.transform(features[col])
 
         # One-hot encoding untuk kolom multikategori
         one_hot_cols = ['person_education', 'loan_intent', 'person_home_ownership']
@@ -42,13 +37,10 @@ class LoanXGBoostModelInference:
 
         # Samakan struktur kolom
         features, _ = features.align(pd.DataFrame(columns=self.columns), join='left', axis=1, fill_value=0)
-        
+
         return features
-    
+
     def predict(self, new_data):
-        # Pastikan kolom 'loan_status' tidak ada di data input
         processed_data = self.preprocess_new_data(new_data)
-        
-        # Prediksi
         prediction = self.model.predict(processed_data)
         return prediction
